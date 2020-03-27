c0407834290e4a70ad3c7b3b75ad0c1ec53b8215
John Kramer <enterprise1117@gmail.com>
2020-03-26 22:53:02 -0500

    updated format, code actually runs in Spyder

--- threebody.py
+++ threebody.py
@@ -8,10 +8,22 @@ from scipy.spatial import distance
 
 # Variables I know I need: and initial conditions  
 
-m = np.array([[1],[2],[3]]) # Mass of bodies
-s0 = np.array([[1,1,1],[2,2,2],[3,3,3]]) # Position of bodies in 2 dimensional plane from the origin [0,0] , [x,y,z] in [m]
-v0 = np.array([[1,1,1],[2,2,2],[3,3,3]]) # Velocity of body A in [m/s]
-a0 = np.array([[1,1,1],[2,2,2],[3,3,3]]) # acceleration of body A in [m/s^2] 
+m = np.array([[1],
+              [2],
+              [3]]) # Mass of 
+
+s0 = np.array([[1,1,1],
+               [2,2,2],
+               [3,3,3]]) # Position of bodies in 2 dimensional plane from the origin [0,0] , [x,y,z]in [m]
+
+v0 = np.array([[1,1,1],
+               [2,2,2],
+               [3,3,3]]) # Velocity of body A in [m/s]
+
+a0 = np.array([[1,1,1],
+               [2,2,2],
+               [3,3,3]]) # acceleration of body A in [m/s^2] 
+
 t0 = 0 # initial time in seconds for math. 
 
 p = m*v0  # initial momentum of bodies
@@ -22,7 +34,7 @@ p = m*v0  # initial momentum of bodies
 # Impulse dp = (F)*dt
 # gravitational attraction F = G*(m1xm2)/r where r is distance between bodies and G = 6.67x10^-11 [N*m^2/kg^2]
 
-G = 6.67*10^-11
+G = 6.67E-11
 
 # OK let's do this, 1st step.
 
